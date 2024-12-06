f5abdcc925dfab59960bcfc90ed6ea00215144bf
Dhanushya V S <75406996+Dhanushya77@users.noreply.github.com>
2024-12-06 16:30:07 +0530

    Delete phonebook directory

--- phonebook/app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Phonebook(models.Model):
-    name = models.TextField()
-    phn_num = models.IntegerField()
--- phonebook/app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from . models import *
-
-class Phonebook_serializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Phonebook
-        fields = '__all__'
